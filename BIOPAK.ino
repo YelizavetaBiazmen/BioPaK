ab5b5cdc95f1c95259753689f16f24e1cd8ffba3
YelizavetaBiazmen <95139780+YelizavetaBiazmen@users.noreply.github.com>
2023-05-13 20:40:29 +0200

    Update BIOPAK.ino

--- BIOPAK.ino
+++ BIOPAK.ino
@@ -1,11 +1,15 @@
 //////////////////////////////////////////////////////////
 //importujemy bibliotekę
-#include "Arduino.h" // tu
-#include "RGBLed.h" // i tu
+#include "Arduino.h" 
+#include "RGBLed.h" 
 // definiojemy stałe
+
+//następne trzy są dla kontroli LED
 #define LEDSTRIPRGB_PIN_SIGB  3 
 #define LEDSTRIPRGB_PIN_SIGR  9
 #define LEDSTRIPRGB_PIN_SIGG  11
+
+
 #define BUTTON_PIN 13 // button pin
 #define LedStripRGB_TYPE COMMON_CATHODE
 
@@ -29,17 +33,21 @@ void setup() {
     //pinMode(SOUND_PIN,INPUT);
     //pinMode(LIGHTS_PIN, OUTPUT);
     pinMode(LIGHTS_SENSOR_PIN, INPUT);
-    for(byte i = 0; i < 3; i++) {
+    
+    //cykl dla otrzymania pinów kolejych LEDów, pokazujących binarny numer trybu
+    for(byte i = 0; i <LED_QTY ; i++) {
       pinMode(LED_PIN_START + i, OUTPUT);//odczytujemy znaczenia 
       
     }
     
     Serial.begin(9600); 
-    LedStripRGB.turnOff();              
+    LedStripRGB.turnOff();  //wyłączamy LED na początku działania programy            
 }
 
 byte changeMode(byte mode = 0){
-  oV =  bV;
+  oV =  bV; // wpisujemystare znaczenie do innej zmiennej, przed tym, jak ją zastąpi inne znaczenie
+    //robimy to dla porównania znaczeń i następnego rozumienia, czy został nacisnięty przecisk 
+    
   bV=digitalRead(BUTTON_PIN);// read the level value of pin 7 and assign if to val
   Serial.println(bV); // pokazuje dane na ekranie
    
@@ -49,8 +57,8 @@ byte changeMode(byte mode = 0){
     if(mode >= MAX_MODES){// jeśli kliknęliśmy przecisk więcej razy, niż jest trybów, znowu idzie zerowy
       mode = 0;
     }
-    delay(100); // czeka
-    Serial.println(mode);// pokazuje dane na ekranie
+    delay(100);
+    Serial.println(mode);
   } 
   return mode;
 }
@@ -63,9 +71,9 @@ void mode3LightsOn(byte mode) {
 
 // pierwsy tryb włącza LED i daje możliwość zmienić jasność
 void mode1AdjustableLights(byte mode) {
-  int ledRoundOld = ledRound;
+  int ledRoundOld = ledRound; // wpisujemystare znaczenie do innej zmiennej, przed tym, jak ją zastąpi inne znaczenie
   int val=analogRead(POTEN_PIN);// read the analog value from the sensor and assign it to val
-  ledRound = round((val/10.0)*2.55);
+  ledRound = round((val/10.0)*2.55); //unikamy migania LEDa, jeśli już jest ciemno, ale nie do końca
   ledRound = round((ledRound + ledRoundOld*7)/8);
   ledRound = ledRound > 250 ? 255 : ledRound;
   Serial.println(ledRound);// display value of val // pokazuje dane na ekranie
@@ -75,6 +83,8 @@ void mode1AdjustableLights(byte mode) {
 
 // drugi tryb włącza LED, tylko jeśli widzi światło
 void mode2LightSensorLights(byte mode) {
+    
+    //jeśli sensor światła widzi światło, to wyłączamy LED
   if (digitalRead(LIGHTS_SENSOR_PIN) == HIGH) {
       //digitalWrite (LIGHTS_PIN, LOW);
       LedStripRGB.turnOff();   
